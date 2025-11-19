84455b307aec29cdf7256352b52b62ed70c6b8d0
Loric Brevet <loric.brevet@gmail.com>
2025-11-19 12:21:06 +0100

    Make SSE example more readable

--- examples/sse/main.go
+++ examples/sse/main.go
@@ -5,33 +5,27 @@ import (
 	"log"
 	"net/http"
 	"strconv"
-	"time"
 
 	"github.com/lobre/eventbus"
 )
 
 func main() {
 	bus := eventbus.New()
-	seed(bus)
+
+	var last uint64
+	last, _ = bus.Publish("notifications", "Ping", last, "hello")
+	bus.Publish("notifications", "Ping", last, "world")
 
 	http.HandleFunc("/events", func(w http.ResponseWriter, r *http.Request) {
 		lastID := parseLastEventID(r.Header.Get("Last-Event-ID"))
 		sub, _ := bus.Subscribe("notifications", lastID, eventbus.BufferDefault)
 		defer sub.Close()
 
-		setupSSEHeaders(w)
-		flusher, _ := w.(http.Flusher)
-
+		sse, _ := newSSE(w)
 		for {
 			select {
-			case e, ok := <-sub.C:
-				if !ok {
-					return
-				}
-				writeSSE(w, e)
-				if flusher != nil {
-					flusher.Flush()
-				}
+			case e := <-sub.C:
+				sse.Write(e)
 			case <-r.Context().Done():
 				return
 			}
@@ -39,15 +33,7 @@ func main() {
 	})
 
 	http.HandleFunc("/publish", func(w http.ResponseWriter, r *http.Request) {
-		msg := r.URL.Query().Get("msg")
-		if msg == "" {
-			msg = "tick"
-		}
-		payload := map[string]string{
-			"msg": msg,
-			"ts":  time.Now().Format(time.RFC3339),
-		}
-		bus.Publish("notifications", "Ping", bus.LastID(), payload)
+		bus.Publish("notifications", "Ping", bus.LastID(), "tick")
 		w.WriteHeader(http.StatusAccepted)
 	})
 
@@ -55,21 +41,26 @@ func main() {
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
-func seed(bus *eventbus.Bus) {
-	var last uint64
-	last, _ = bus.Publish("notifications", "Ping", last, map[string]string{"msg": "hello"})
-	bus.Publish("notifications", "Ping", last, map[string]string{"msg": "world"})
+type sseWriter struct {
+	w     http.ResponseWriter
+	flush func()
 }
 
-func setupSSEHeaders(w http.ResponseWriter) {
+func newSSE(w http.ResponseWriter) (*sseWriter, error) {
 	w.Header().Set("Content-Type", "text/event-stream")
 	w.Header().Set("Cache-Control", "no-cache")
+	flusher, ok := w.(http.Flusher)
+	if !ok {
+		return nil, fmt.Errorf("response writer does not support flushing")
+	}
+	return &sseWriter{w: w, flush: flusher.Flush}, nil
 }
 
-func writeSSE(w http.ResponseWriter, e eventbus.Event) {
-	fmt.Fprintf(w, "id: %d\n", e.ID)
-	fmt.Fprintf(w, "event: %s\n", e.Type)
-	fmt.Fprintf(w, "data: %v\n\n", e.Payload)
+func (s *sseWriter) Write(e eventbus.Event) {
+	fmt.Fprintf(s.w, "id: %d\n", e.ID)
+	fmt.Fprintf(s.w, "event: %s\n", e.Type)
+	fmt.Fprintf(s.w, "data: %v\n\n", e.Payload)
+	s.flush()
 }
 
 func parseLastEventID(raw string) uint64 {
